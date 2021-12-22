e938f500be0bf64a0e5467c2062a7b34a9667e3b
Jose Angel Hernandez <20171006@uthh.edu.mx>
2021-12-22 02:01:57 -0600

    ADD proyecto basico cascaron para implementar proyectos usanmdo webpack

---
+++ src/js/componentes.js
@@ -0,0 +1,17 @@
+/* importar css desde componente js */
+import '../css/componentes.css';
+/* import webpackLogo from '../assets/img/webpack-logo.png'; */
+
+export const saludar = (nombre) => {
+        console.log("creando etiqueta h1");
+        const h1 = document.createElement("h1");
+        h1.innerText = `Hola soy ${nombre} ***!!!`;
+        document.body.append(h1);
+
+        /*  console.log(webpackLogo);
+
+         const img = document.createElement("img");
+         img.src = webpackLogo;
+         document.body.append(img); */
+    }
+    /* export default saludar; */
